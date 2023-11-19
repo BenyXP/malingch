e3b1603f1c5da0f5841104004a1fd9cdb970f703
BenyXP <103335260+BenyXP@users.noreply.github.com>
2023-11-19 16:54:36 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = []
+API_ID = 28414908
+API_HASH = "8d2e5bdb8848ae0b37e869be445a8f32"
+BOT_TOKEN = "6596482531:AAHmCTDtxYMflOgT4hmSV7LBNqkEh8IQGrE"
+SESSION = "AQAhIHQAZpPbeZrVmZxDble8MiIPmbVmGmbBcOx4fHzxJCgeBinsLYzS3l3tppwmHv5XeDgmeGSzVFf8z7McSTEH4YLUAgXkisk44Tn5t0dsTjrQiFVbQWeZyurR0Hja5egYhI0NkwdXPoV9GAbckQGhBrvNFdqE4ws_hQ6vwY7agE8Q2xLQoC-MnZl8xzEl2VoxFPm0jk4Jgr0AeHrZJkkaKn2VVjRE6NQqJsKc3tEBnrTAOUiUC2BjJz9XQPsEZbo3seauUQo_umRU0DTiNJ-MuFtEgezCd7ejW6tMSPYiDnZH1tpk03h6ur5MY5LcHGMSkZkiW1Zr5jMvLZUQqvO7PqESswAAAABuJAClAA"
+FORCESUB = "lepinst4r"
+AUTH = "1847853221"
 
 auth = config("AUTH", default=None, cast=int)
 ids = auth.split(",")
